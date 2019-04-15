8060d9692a28de00e5d3c370498a2a39c4fd8ea6
Lon <gavinlondee@gmail.com>
2019-04-15 17:48:26 +0200

    Initial commit

---
+++ MusicianClassExample/Musician.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MusicianClassExample
+{
+    internal class Musician
+    {
+        public enum Instrument
+        {
+            guitar,
+            piano,
+            bass,
+            drums
+        }
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+
+                if (value.Length < 3)
+                {
+                    throw new Exception("FirstName must be greater than 2 characters");
+                }
+                _firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new Exception("LastName must be greater than 2 characters");
+                }
+                _lastName = value;
+            }
+        }
+        public Instrument MusicalIntrument
+        {
+            get
+            {
+                return _instrument;
+            }
+            set
+            {
+                _instrument = value;
+            }
+        }
+
+        private static int _id = 0;
+        private string _firstName = String.Empty;
+        private string _lastName = string.Empty;
+        private Instrument _instrument = Instrument.guitar;
+
+        public event EventHandler Finished;
+
+        public Musician(string firstName, string lastName, Instrument instrument)
+        {
+            _id++;
+            FirstName = firstName;
+            LastName = lastName;
+            MusicalIntrument = instrument;
+        }
+
+        public void Play()
+        {
+            Console.WriteLine($"{this} is playing the {MusicalIntrument}");
+            Thread.Sleep(7000);
+
+            Finished?.Invoke(this, null);
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: {FirstName} {LastName}";
+        }
+
+    }
+}
---
+++ MusicianClassExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MusicianClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Musician musician1 = new Musician("Kevin", "Jones", Musician.Instrument.guitar);
+                musician1.Finished += Musician_Finished;
+                musician1.Play();
+
+                Musician musician2 = new Musician("Bob", "Harris", Musician.Instrument.piano);
+                musician2.Finished += Musician_Finished;
+                musician2.Play();
+
+                Musician musician3 = new Musician("Sarah", "Harris", Musician.Instrument.drums);
+                musician3.Finished += Musician_Finished;
+                musician3.Play();
+
+                Musician musician4 = new Musician("Stephan", "Brown", Musician.Instrument.bass);
+                musician4.Finished += Musician_Finished;
+                musician4.Play();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("-End-");
+            }
+
+
+            Console.ReadKey();
+        }
+
+        private static void Musician_Finished(object sender, EventArgs e)
+        {
+            Console.WriteLine($"{sender} has finished playing");
+        }
+
+    }
+}
